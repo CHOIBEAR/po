8fac43b19f638b10d2eae49130eb0503efa7fad0
CHOIBEAR <dgsb1406@gmail.com>
2025-10-23 09:45:36 +0900

    Add files via upload

--- portfolio-data.js
+++ portfolio-data.js
@@ -11,9 +11,9 @@
 
 var portfolioItems = [
   {
-    title: '프로젝트 시연영상',
-    description: 'hideu 팀 개발 프로젝트 시연영상 입니다.',
-    image: 'video-placeholder.png',
+    title:'프로젝트 시연영상',
+    description: 'hideu 팀 개발 프로젝트 시연영상 입니다. 팀장과 제작총괄을 맡았습니다.',
+    image:'video-placeholder.png',
     url:'https://youtu.be/VmUwMCsS5V8'
   },
   {
